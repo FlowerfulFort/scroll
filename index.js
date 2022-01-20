0f5066d69981077ebf4e40c50a62fd160ae7e9ef
FlowerfulFort <skagyghk@naver.com>
2022-01-20 15:04:29 +0900

    Added electron back-end

---
+++ index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+const openMainWindow = () => {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        webPreferences: {
+            preload: path.resolve(__dirname, 'src', 'preload.js'),
+        },
+    });
+    win.loadFile('dist/index.html');
+    return win;
+};
+
+app.whenReady().then(() => {
+    let window = openMainWindow();
+});
+
+/* in macOS, call quit() manually */
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+/* if Program started at first time. */
+try {
+    if(!fs.existsSync('data')) { // if not exists 'data' directory
+        fs.mkdirSync('data');
+    }
+    if(!fs.existsSync('data/data.yml')) {   // if not exists 'data/data.yml' file
+        fs.writeFileSync('./data/data.yml', "");
+    }
+} catch (err) {
+    console.error(err);
+}
