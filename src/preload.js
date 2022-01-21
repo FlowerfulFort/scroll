6b6d0389ee3e99f11a2683f0865e0a28b9b4b154
FlowerfulFort <skagyghk@naver.com>
2022-01-21 10:39:19 +0900

    Added writing bridge

--- src/preload.js
+++ src/preload.js
@@ -5,8 +5,24 @@ console.log("preload.js loaded.");
 
 contextBridge.exposeInMainWorld('load',
 {
-    loadData: () => {
-        let rawdata = fs.readFileSync('./data/data.yml', 'utf8');
+    loadData: () => {   // send Object from yml file.
+        let rawdata;
+        try {
+            rawdata = fs.readFileSync('./data/data.yml', 'utf8');
+        } catch (err) {
+            console.error(err);
+        }
         return yaml.load(rawdata);
+    }
+});
+contextBridge.exposeInMainWorld('write',
+{
+    writeData: (raw) => {   // write Object Data to yml file.
+        let serialized = yaml.dump(raw);
+        try {
+            fs.writeFileSync('./data/data.yml', serialized, 'utf8');
+        } catch (err) {
+            console.error(err);
+        }
     }
 });
