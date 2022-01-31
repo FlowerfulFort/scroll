7552116d691fbcd79a2c20d890e3c3da1695cf2e
FlowerfulFort <skagyghk@naver.com>
2022-01-31 10:43:56 +0900

    Modified FooterPage

--- src/FooterHalfPage.jsx
+++ src/FooterHalfPage.jsx
@@ -3,11 +3,19 @@ import styled from "styled-components";
 
 const FlexContainer = styled.div`
     display: flex;
+    height: max-content;
 `;
 
 const FlexChildren = styled.div`
     flex-grow: 1;
     flex-basis: 0px;
+    &:nth-child(1) {
+        padding-right: 3px;
+        border-right: gray solid 1px;
+    }
+    &:nth-last-child(1) {
+        margin-left: 3px;
+    }
 `;
 export default function FooterHalfPage({ children }) {
     return (
