c7bcb2eedb0093ae86edd5067548c4f8e841438c
FlowerfulFort <skagyghk@naver.com>
2022-01-31 11:24:42 +0900

    ParseDay function bug fixed

--- src/ParseDay.js
+++ src/ParseDay.js
@@ -1,4 +1,4 @@
 /* This function parses Date Object & returns day string */
 export default function ParseDay(DayObj) {
-    return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(Dayobj);
+    return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(DayObj);
 };
