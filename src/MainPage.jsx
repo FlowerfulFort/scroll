dad1058c110b2cb5695fda87f55af99bd5fb1e17
FlowerfulFort <skagyghk@naver.com>
2022-02-02 14:56:11 +0900

    MainPage Updated

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -10,6 +10,11 @@ const MainFooter = styled.div`
     text-align: center;
     padding-top: 2px;
     padding-bottom: 3px;
+    background-color: skyblue;
+`;
+const Title = styled(MainFooter)`
+    border-bottom: black solid 2px;
+    background-color: white !important;
 `;
 export default class MainPage extends React.PureComponent {
     constructor(props) {
@@ -21,6 +26,7 @@ export default class MainPage extends React.PureComponent {
         };
         this.nextDate = this.state.referDate + 1;
         this.refresh = setInterval(this.checkDay, 5000);
+
         console.log(`Today is ${this.referDateObj}`);
 
         this.obj = window.load.loadData();
@@ -29,16 +35,30 @@ export default class MainPage extends React.PureComponent {
         const now = new Date().getDate();
         const oldDate = this.state["referDate"];
         if (this.state["referDate"] !== now) {
+            this.referDateObj = new Date();
             this.setState({ referDate: now }, () => {
                 console.log(`Date changed ${oldDate} to ${now}`);
-                this.nextDate = this.state["referDate"] + 1;
+                this.nextDate = this.state["referDate"] + 1; // setState() 비동기 작동에 의한 오작동 가능성.
             });
         }
+    }
+    Date2String() {
+        return `${
+            this.referDateObj.getMonth() + 1
+        }. ${this.referDateObj.getDate()}. ${this.referDateObj.getFullYear()} ${ParseDay(
+            this.referDateObj
+        )}`;
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log(
+            `componentDidUpdate occured.\nprevState: ${prevState} now: ${this.state}`
+        );
     }
     render() {
         console.log("MainPage Rendered");
         return (
             <React.Fragment>
+                <Title>{this.Date2String()}</Title>
                 <HalfPage>
                     <MainFooter>Today's Task</MainFooter>
                     <MainFooter>Tomorrow's Task</MainFooter>
