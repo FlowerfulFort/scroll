90574f92957b8b9fb07c524bca264114725c389c
FlowerfulFort <skagyghk@naver.com>
2022-02-05 11:59:40 +0900

    Fixed DailyTasks render

--- src/DailyTasks.jsx
+++ src/DailyTasks.jsx
@@ -1,8 +1,5 @@
 /* Component: DailyTasks
  * DailyTasks is a Page for viewing daily tasks.
- * 알려진 버그:
- * - 날짜가 바뀌어 props가 바뀌면 render()가 실행되긴 하지만
- *   this.daily(weekly)list가 바뀌지 않아 화면은 그대로이다.
  */
 import React from "react";
 import TaskBlock from "./TaskBlock";
@@ -13,23 +10,29 @@ import MergeDailyWeekly from "./MergeDailyWeekly";
 class DailyTasks extends React.PureComponent {
     constructor(props) {
         super(props);
+        /*
         this.dailylist = this.props.obj["daily"];
 
         this.referObj = new Date().setDate(this.props.referDate);
         const today = ParseDay(this.referObj);
         const weekly = this.props.obj["weekly"].filter((e) => e["day"] === today);
+        */
         /*
         this.props.obj["weekly"].forEach((element) => {
             if (element["day"] === today) weekly.push(element);
         });
         */
-        this.weeklylist = weekly;
     }
     render() {
+        const dailylist = this.props.obj["daily"];
+
+        const referObj = new Date().setDate(this.props.referDate);
+        const today = ParseDay(referObj);
+        const weeklylist = this.props.obj["weekly"].filter((e) => e["day"] === today);
         console.log("DailyTasks Rendered");
         return (
             <div className="TasksPage">
-                {MergeDailyWeekly(this.dailylist, this.weeklylist).map((element) => {
+                {MergeDailyWeekly(dailylist, weeklylist).map((element) => {
                     return (
                         <TaskBlock
                             taskname={element["name"]}
